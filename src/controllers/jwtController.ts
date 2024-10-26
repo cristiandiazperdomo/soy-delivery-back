bf3e5acd7642ed35956ac54d895ab315044b10d8
cristiandiazperdomo <danubiodiaz2003edf@gmail.com>
2024-10-26 10:05:31 -0300

    jwt handling errors

--- src/controllers/jwtController.ts
+++ src/controllers/jwtController.ts
@@ -1,28 +1,24 @@
 import {Request, Response} from "express";
 import {jwtService} from "../services/jwtService";
-import {JsonWebTokenError, TokenExpiredError} from "jsonwebtoken";
+import {TokenExpiredError} from "jsonwebtoken";
 
 export const jwtController = {
     getUserFromToken: (req: Request, res: Response) => {
         const token = req.headers.authorization?.replace("Bearer ", "");
 
         try {
-            if (token === undefined) {
+            if (!token) {
                 throw new Error("Token is undefined");
             }
             res.json(jwtService.getUserFromToken(token));
         } catch (error) {
             if (error instanceof Error) {
+                let errorInfo: string = error.name + ": " + error.message;
                 if (error instanceof TokenExpiredError) {
-                    res.status(401).send(
-                        error.name +
-                            ": " +
-                            error.message +
-                            " " +
-                            error.expiredAt.toLocaleString()
-                    );
-                } else if (error instanceof JsonWebTokenError) {
-                    res.status(400).send(error.name + ": " + error.message);
+                    errorInfo += " " + error.expiredAt.toLocaleString();
+                    res.status(401).send(errorInfo);
+                } else {
+                    res.status(400).send(errorInfo);
                 }
             }
         }
